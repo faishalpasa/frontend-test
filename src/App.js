4be4b5a85ae703943692763f4986928231a02ec7
Pasa <mfaishal_pasa@hotmail.com>
2021-03-29 18:42:13 +0700

    [ADD] initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components'
+
+import TopBar from './components/TopBar'
+import './App.css';
+
+const Main = styled.main`
+  background-color: #FFFFFF;
+`
+const Header = styled.header`
+  background: #031D5B;
+`
+
+function App() {
+  useEffect(() => {
+    fetch('https://329650a9-72c1-438e-96dd-00c1bb810a1f.mock.pstmn.io/takehometest/frontend/web/orange')
+    .then(res => res.json())
+    .then((response) => {
+      console.log(response)
+    })
+    console.log('test')
+  }, [])
+  
+  return(
+    <Main>
+      <Header>
+        <TopBar />
+      </Header>
+    </Main>
+  );
+}  
+
+export default App
---
+++ src/components/TopBar/TopBarView.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TopBar = styled.div`
+  padding: 25px 120px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #ffffff;
+`
+const Logo = styled.p`
+  font-weight: 900;
+  font-size: 26px;
+  line-height: 58px;
+  margin: 0;
+`
+const Navigation = styled.nav`
+  padding: 0;
+  margin: 0;
+  display: flex;
+  gap: 16px;
+`
+const NavigationItem = styled.a`
+
+`
+
+function NavigationView() {
+  return(
+    <TopBar>
+      <Logo>
+        ORANGE
+      </Logo>
+      <Navigation>
+        <NavigationItem>
+          Benefit
+        </NavigationItem>
+        <NavigationItem>
+          Pricing
+        </NavigationItem>
+        <NavigationItem>
+          Testimonial
+        </NavigationItem>
+      </Navigation>
+    </TopBar>
+  );
+}  
+
+export default NavigationView
